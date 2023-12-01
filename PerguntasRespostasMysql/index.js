e196da7ac45bc979ff45e90e5a1d11f70ec301fe
pauloceami <pauloceami@gmail.com>
2023-12-01 03:04:16 -0300

    adicionando bootstrap 5 0 2 framework css no projeto

--- PerguntasRespostasMysql/index.js
+++ PerguntasRespostasMysql/index.js
@@ -7,38 +7,8 @@ app.set('view engine', 'ejs');
 // permite usar arquivos estaticos
 app.use(express.static('public'));
 
-app.get('/:nome?', function (req, res) {
-
-    var nome = req.params.nome;
-
-    res.render('staticfile', {
-        nome: nome,
-        is_mostrar: false,
-        produtos: [
-            {
-                produto_id: 1,
-                produto_nome: 'doritos',
-                produto_preco: 5.99,
-                produto_estoque: 15
-
-            },
-            {
-                produto_id: 2,
-                produto_nome: 'rufles',
-                produto_preco: 7.99,
-                produto_estoque: 25
-
-            },
-            {
-                produto_id: 3,
-                produto_nome: 'fandangos',
-                produto_preco: 10.99,
-                produto_estoque: 36
-
-            }
-        ]
-
-    });
+app.get('/', function (req, res) {
+    res.render('home');
 })
 
 
