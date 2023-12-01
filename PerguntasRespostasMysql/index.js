ea22bd489233c655aefa9618c41d328566a6cc55
pauloceami <pauloceami@gmail.com>
2023-11-30 22:56:19 -0300

    trabalhando com foreach no ejs

--- PerguntasRespostasMysql/index.js
+++ PerguntasRespostasMysql/index.js
@@ -8,9 +8,33 @@ app.get('/:nome?', function (req, res) {
 
     var nome = req.params.nome;
 
-    res.render('condicionais', {
+    res.render('foreach', {
         nome: nome,
-        is_mostrar: false
+        is_mostrar: false,
+        produtos: [
+            {
+                produto_id: 1,
+                produto_nome: 'doritos',
+                produto_preco: 5.99,
+                produto_estoque: 15
+
+            },
+            {
+                produto_id: 2,
+                produto_nome: 'rufles',
+                produto_preco: 7.99,
+                produto_estoque: 25
+
+            },
+            {
+                produto_id: 3,
+                produto_nome: 'fandangos',
+                produto_preco: 10.99,
+                produto_estoque: 36
+
+            }
+        ]
+
     });
 })
 
